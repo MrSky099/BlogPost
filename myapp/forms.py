98db371a30d0354c06963963c5f27c1a7c31888c
Akash Patel <akashpatel1602906@gmail.com>
2024-03-30 21:48:16 +0530

    changing in front end

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,8 +4,8 @@ from .models import UserBlogs
 class UserBlogsForm(forms.ModelForm):
     class Meta:
         model = UserBlogs
-        fields = ['Title','BlogImages','BlogContent', 'author']
+        fields = ['Title','BlogImages','BlogContent']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['BlogImages'].widget.attrs.update({'accept': 'image/*'})
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['BlogImages'].widget.attrs.update({'accept': 'image/*'})
